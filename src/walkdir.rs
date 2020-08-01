b5126da72d9489f0c63f1da854a9e0c8462dd3e1
yasukotelin <31115673+yasukotelin@users.noreply.github.com>
2020-08-01 19:47:18 +0900

    Ignore hidden directory.

--- src/walkdir.rs
+++ src/walkdir.rs
@@ -2,13 +2,12 @@ use std::path::PathBuf;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
 
-const IGNORES: [&str; 6] = [
+const IGNORES: [&str; 5] = [
     "node_modules",
     "build",
+    "target",
     "out",
-    ".idea",
-    ".vscode",
-    ".gradle",
+    "bin"
 ];
 
 pub fn walk_projects(dirs: Vec<PathBuf>) -> Vec<PathBuf> {
@@ -18,16 +17,26 @@ pub fn walk_projects(dirs: Vec<PathBuf>) -> Vec<PathBuf> {
         .collect::<Vec<PathBuf>>()
 }
 
+// Skip when directory is finded ignores or "." directory.
 fn is_skip(entry: &DirEntry) -> bool {
     let path = entry.path();
-    let file_name = path.file_name().unwrap();
+    let file_name = path.file_name().unwrap().to_str().unwrap();
     if path.is_dir() {
+        // check started with "." hidden directory.
+        if is_hidden_file(file_name) {
+            return false;
+        }
+        // check ignores
         match IGNORES.iter().find(|&&v| v == file_name) {
             Some(_) => return true,
             None => return false,
         };
     }
     return false;
+}
+
+fn is_hidden_file(file_name: &str) -> bool {
+    file_name.starts_with(".")
 }
 
 fn is_git_project(entry: &DirEntry) -> bool {
@@ -43,12 +52,16 @@ fn walk_project(dir: &PathBuf) -> Vec<PathBuf> {
     let mut project_paths: Vec<PathBuf> = Vec::new();
     let walker = WalkDir::new(dir).into_iter();
     for _ in walker.filter_entry(|e| {
-        if is_skip(e) {
-            return false;
-        }
         if is_git_project(e) {
             project_paths.push(e.path().parent().unwrap().to_path_buf());
             return false;
+        }
+        if is_skip(e) {
+            return false;
+        }
+
+        if e.path().is_dir() {
+            println!("{}", e.file_name().to_str().unwrap());
         }
         return true;
     }) {}
