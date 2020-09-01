a3dd5d18eda4509363ab6df9df848ef9df1ff189
yasukotelin <31115673+yasukotelin@users.noreply.github.com>
2020-09-02 02:05:51 +0900

    Replace with Rust2018 module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 pub mod config;
-pub mod walkdir;
-pub mod utils;
 pub mod skim;
+pub mod utils;
+pub mod walkdir;
+
+#[macro_export]
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
